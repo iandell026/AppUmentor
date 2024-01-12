ffa7f9c1c75c6731a16ce2220ef29bd10e0fa248
Iandell Lorenzon <iandell.rl@gmail.com>
2024-01-12 13:14:37 -0300

    Adicionado Form de Cadastro em view separada
    Separado o código do formulário de cadastro em uma nova view, para melhor organização da view de listagem

---
+++ app/Views/cadastro.php
@@ -0,0 +1,19 @@
+<form id="formUsuario">
+    <div class="row">
+        <div class="col-md-4 mb-3">
+        <label for="nome" class="form-label">Nome</label>
+        <input type="text" class="form-control" id="nome" name="nome">
+        </div>
+
+        <div class="col-md-4 mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email">
+        </div>
+
+        <div class="col-md-4 mb-3">
+        <label for="dataAdmissao" class="form-label">Data de Admissão</label>
+        <input type="date" class="form-control" id="dataAdmissao" name="dataAdmissao">
+        </div>
+    </div>
+    <button type="button" class="btn btn-primary mb-2" id="adicionarUsuario">Adicionar Usuário</button>
+</form>
--- app/Views/listagem.php
+++ app/Views/listagem.php
@@ -19,26 +19,7 @@
       <div class="row">
         <div class="col-sm-12 mt-5 mb-3">
           <h3>Lista de Usuários</h3>
-          <div id="dadosCadastro">
-            <form id="formUsuario">
-              <div class="row">
-                <div class="col-md-4 mb-3">
-                  <label for="nome" class="form-label">Nome</label>
-                  <input type="text" class="form-control" id="nome" name="nome">
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input type="email" class="form-control" id="email" name="email">
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="dataAdmissao" class="form-label">Data de Admissão</label>
-                  <input type="date" class="form-control" id="dataAdmissao" name="dataAdmissao">
-                </div>
-              </div>
-            </form>
-            <button type="button" class="btn btn-primary mb-2" id="adicionarUsuario">Adicionar Usuário</button>
-          </div>
-
+          <?= view('cadastro') ?>
         </div>
         <div class="table table-responsive">
           <table class="table table-hover table-bordered" id="tabelaUsuarios">
